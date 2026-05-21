9ebbc47f856230e337539becf5ca006fa426f548
CelestePerez1 <celeste.perez@utp.ac.pa>
2026-05-21 11:30:26 -0500

    no

--- Grupo2/FormativoForm_P1/index.php
+++ Grupo2/FormativoForm_P1/index.php
@@ -20,5 +20,19 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
+<script>
+    $(document).ready(function() {
+        $("#formulario").submit(function(e) {
+            var campoNombre = $("input[name='nombre_apellido']");
+            if (campoNombre.val().trim() === "") {
+                alert("El nombre y apellido no puede estar vacío o contener solo espacios.");
+                campoNombre.val("").focus();
+                e.preventDefault();
+                return false;
+            }
+        });
+    });
+</script>
+
 </body>
 </html>
